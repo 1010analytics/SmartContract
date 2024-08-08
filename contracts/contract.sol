b6d89ffe25d58131a230ce2a67f37e73d4fc2f5e
Tathya <data.scientist1010@gmail.com>
2024-08-09 05:08:39 +0530

    Added  emergency withdrawal function

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -13,25 +13,36 @@ contract PrizeVault {
         owner = _owner;
     }
 
+    
     modifier onlyOwner() {
         require(msg.sender == owner, "Not authorized");
         _;
     }
 
+    
     function releasePrize(address payable recipient, uint256 amount) external onlyOwner {
         require(address(this).balance >= amount, "Insufficient funds in vault");
         (bool sent, ) = recipient.call{value: amount}("");
         require(sent, "Failed to send prize");
     }
 
+    
+    function emergencyWithdraw(address payable recipient, uint256 amount) external onlyOwner {
+        require(address(this).balance >= amount, "Insufficient funds");
+        (bool sent, ) = recipient.call{value: amount}("");
+        require(sent, "Failed to send emergency funds");
+    }
+
+    
     receive() external payable {}
 }
+
 
 contract TaxToken is VRFConsumerBase, ReentrancyGuard, AutomationCompatible {
     uint256 public constant BUY_SELL_TAX_PERCENT = 19;
     uint256 public constant DEV_FEE_PERCENT = 1;
     uint256 public constant TOKEN_INITIAL_PRICE = 0.01 ether;
-    uint256 public totalTokenSupply;  
+    uint256 public totalTokenSupply;
     address public devWallet;
     PrizeVault public prizeVault;
     address[] public tokenHolders;
@@ -56,7 +67,7 @@ contract TaxToken is VRFConsumerBase, ReentrancyGuard, AutomationCompatible {
         fee = _fee;
         devWallet = _devWallet;
         prizeVault = new PrizeVault(address(this));
-        totalTokenSupply = 0;  
+        totalTokenSupply = 0;
     }
 
     function buyTokens() public payable nonReentrant {
@@ -66,13 +77,13 @@ contract TaxToken is VRFConsumerBase, ReentrancyGuard, AutomationCompatible {
         uint256 tokensToMint = (msg.value - tax - devFee) / TOKEN_INITIAL_PRICE;
 
         tokenBalances[msg.sender] += tokensToMint;
-        totalTokenSupply += tokensToMint;  
+        totalTokenSupply += tokensToMint;
         totalTaxCollected += tax;
         if (!isTokenHolder(msg.sender)) {
             tokenHolders.push(msg.sender);
         }
         payable(devWallet).transfer(devFee);
-        prizeVault.releasePrize{value: tax}(payable(prizeVault), tax);
+        prizeVault.releasePrize{value: tax}(payable(address(prizeVault)), tax);
     }
 
     function sellTokens(uint256 amount) public nonReentrant {
@@ -85,11 +96,11 @@ contract TaxToken is VRFConsumerBase, ReentrancyGuard, AutomationCompatible {
         uint256 amountAfterTax = totalValue - tax - devFee;
 
         tokenBalances[msg.sender] -= amount;
-        totalTokenSupply -= amount;  
+        totalTokenSupply -= amount;
         totalTaxCollected += tax;
         payable(msg.sender).transfer(amountAfterTax);
         payable(devWallet).transfer(devFee);
-        prizeVault.releasePrize{value: tax}(payable(prizeVault), tax);
+        prizeVault.releasePrize{value: tax}(payable(address(prizeVault)), tax);
     }
 
     function isTokenHolder(address candidate) private view returns (bool) {
